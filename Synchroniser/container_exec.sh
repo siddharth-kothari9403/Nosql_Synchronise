3113061617aceeddc5bef2c7fb0fd0b2d05c5fb3
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2025-04-24 15:08:03 +0530

    Create csv without headers

---
+++ Synchroniser/container_exec.sh
@@ -0,0 +1,3 @@
+mongosh mongodb://myuser:mypassword@localhost:27017
+psql -U myuser -d postgres
+beeline -u 'jdbc:hive2://localhost:10000/'
--- Synchroniser/load_hive_data.sh
+++
@@ -1,1 +0,0 @@
-docker exec -i hive4 beeline -u 'jdbc:hive2://localhost:10000/' -f /opt/hive/init/init_hive.hql
